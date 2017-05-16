57e403acb4bc1b2f09f57cb931dd00e07a997152
Benoît Claveau <benoit.claveau@izzili.com>
2017-05-16 11:58:00 +0200

    View

--- App/index.android.js
+++ App/index.android.js
@@ -3,11 +3,9 @@ import { StackNavigator } from 'react-navigation';
 import HomeScreen from './app/home';
 import DetailsScreen from './app/details';
 import DetailScreen from './app/detail';
-import TabBarScreen from './app/tabbar';
 
 const App = StackNavigator({
-  // Home: { screen: HomeScreen },
-  Home: { screen: TabBarScreen },
+  Home: { screen: HomeScreen },
   Details: { screen: DetailsScreen },
   Detail: { screen: DetailScreen }
 });
