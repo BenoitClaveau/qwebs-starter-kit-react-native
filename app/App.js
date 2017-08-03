28e31a77b53600bcdea3041578157de1d8965e65
BenoitClaveau <https://BenoitClaveau@github.com>
2017-08-03 23:03:20 +0200

    Jest works

--- app/App.js
+++ app/App.js
@@ -1,4 +1,20 @@
-// Used by expo
-import App from './src/app';
+import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import store from './src/redux/store';
+import App from './src/pages/app'; //react-navigation
 
-export default App;
+class ReduxApp extends Component {
+  constructor(){
+    super();
+  }
+
+  render() {
+    return (
+      <Provider store={store}>
+        <App />
+      </Provider>
+    );
+  }
+}
+
+export default ReduxApp;
